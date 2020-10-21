dc8248f7776df9f2e6b4bc5eaaf6cfd50a744250
RoninEMH <60003921+RoninEMH@users.noreply.github.com>
2020-10-21 15:50:48 +0300

    adding to other branch

---
+++ precache-manifest.8817755b8b52761a8b812655792f6576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a443fc841915211ef007d12736a8213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "026295a5c50c85558494",
+    "url": "/static/css/main.12d592d1.chunk.css"
+  },
+  {
+    "revision": "d31f901703b775e5ff80",
+    "url": "/static/js/2.31377156.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31377156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026295a5c50c85558494",
+    "url": "/static/js/main.9e7b08d3.chunk.js"
+  },
+  {
+    "revision": "ead25b3f871ab0029a85",
+    "url": "/static/js/runtime-main.68c74421.js"
+  }
+]);
